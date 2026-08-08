598b5abe95cf888e32da729b1247e0015c20d2d3
Manthan Kadu <kadumanthan05@gmail.com>
2026-08-08 23:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (69.43%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+
+        while (word.size() < k) {
+            int n = word.size();
+
+            for (int i = 0; i < n; i++) {
+                word += (word[i] - 'a' + 1) % 26 + 'a';
+            }
+        }
+
+        return word[k - 1];
+    }
+};
